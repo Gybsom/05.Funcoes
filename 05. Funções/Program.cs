68cb27c3080c821866b56ec35acb1dbaca8390c5
Gybsom Souza <gybsom.souza@gmail.com>
2023-07-04 14:48:09 -0300

    Adicionar arquivos de projeto.

---
+++ 05. Funções/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Informe um número inteiro:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        int numeroTriplicado = TriplicaNumero(numero);
+        Console.WriteLine("O triplo do número " + numero + " é " + numeroTriplicado);
+        Console.WriteLine(numero);
+    }
+
+    static int TriplicaNumero (int numero)
+    {
+        numero = numero * 3;
+        
+        return numero;
+    }
+}
